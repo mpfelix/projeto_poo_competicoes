530291e928737f7dbb096327ec8de34fa172af7c
Maria Paula <felizmariapaula08@gmail.com>
2025-09-09 01:09:22 -0300

    continuação

--- model/prova_teorica.py
+++
@@ -1,9 +0,0 @@
-from .prova import Prova
-
-class ProvaTeorica(Prova):
-    def __init__(self, id, nome, pontuacao_maxima, numero_questoes):
-        super().__init__(id, nome, pontuacao_maxima)
-        self.numero_questoes = numero_questoes
-
-    def calcular_pontuacao(self, acertos):
-        return (acertos / self.numero_questoes) * self.pontuacao_maxima
